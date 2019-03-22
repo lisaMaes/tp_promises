2f087f9dae2f3c7798bb3db648a5a1224fc165b3
Lisa <maestrilisa@yahoo.f>
2019-03-22 17:07:48 +0100

    promise avec .then et .catch

---
+++ index.js
@@ -0,0 +1,42 @@
+const fse = require('fs-extra');
+const fs = require('fs');
+const tmp = './temp';
+const oldFile = './pubs.json';
+const newFile = './temp/pubs.json';
+
+
+
+
+// Promise usage:
+fse.pathExists(tmp)
+    .then(exists =>
+        fse.remove('/tmp/myfile', err => {
+        if (err) return console.error(err)
+
+        console.log('removed !')
+            fse.ensureDir(tmp)
+                .then(() => {
+                    console.log('success DIr!');
+                    // With Promises:
+                    fse.copy(oldFile, newFile)
+                        .then(() => {
+                            console.log('success!')
+
+                            fs.watchFile('message.text', (curr, prev) => {
+                                console.log(`the current mtime is: ${curr.mtime}`);
+                                console.log(`the previous mtime was: ${prev.mtime}`);
+                            });
+
+                        })
+                        .catch(err => {
+                            console.error(err)
+                        })
+                })
+                .catch(err => {
+                    console.error(err)
+                })
+    })
+)
+
+
+
