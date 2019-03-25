32cbb3156407fbc281f500eed534ccf87594b992
Lisa <maestrilisa@yahoo.f>
2019-03-25 10:27:59 +0100

    ajout des await

--- index.js
+++ index.js
@@ -3,17 +3,13 @@ const fs = require('fs');
 const tmp = './temp';
 const oldFile = './pubs.json';
 const newFile = './temp/pubs.json';
-
-
-
-
 // Promise usage:
+/*
 fse.pathExists(tmp)
     .then(exists =>
         fse.remove('/tmp/myfile', err => {
-        if (err) return console.error(err)
-
-        console.log('removed !')
+            if (err) return console.error(err)
+            console.log('removed !')
             fse.ensureDir(tmp)
                 .then(() => {
                     console.log('success DIr!');
@@ -21,12 +17,10 @@ fse.pathExists(tmp)
                     fse.copy(oldFile, newFile)
                         .then(() => {
                             console.log('success!')
-
                             fs.watchFile('message.text', (curr, prev) => {
                                 console.log(`the current mtime is: ${curr.mtime}`);
                                 console.log(`the previous mtime was: ${prev.mtime}`);
                             });
-
                         })
                         .catch(err => {
                             console.error(err)
@@ -35,8 +29,34 @@ fse.pathExists(tmp)
                 .catch(err => {
                     console.error(err)
                 })
+        })
+    )
+*/
+
+fse.pathExists(tmp)
+    .then(exists =>{
+
+        if(exists){
+            return fse.remove('/tmp/myfile');
+        }
+        return ;
     })
-)
+    .then(() =>{
+        return fse.ensureDir(tmp);
 
+    })
+    .then(() =>{
+        return  fse.copy(oldFile, newFile);
 
+    })
+    .then(() =>{
+        return fs.watchFile('message.text', (curr, prev) => {
+            console.log(`the current mtime is: ${curr.mtime}`);
+            console.log(`the previous mtime was: ${prev.mtime}`);
+        });
+    })
+
+    .catch(err => {
+        console.error(err)
+    })
 
